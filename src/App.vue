90a896e5aa2f93ecae9835d071475b3da6bab2e9
olegsugatov <oleg.sugatov@gmail.com>
2020-11-17 23:00:47 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+      <PostForm/>
+      <h1>{{postsCount}}</h1>
+      <div class='post' v-for='post in validPosts' :key='post.id'>
+        <h2>{{post.title}}</h2>
+        <p>{{post.body}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+// helper для упрощения получения getters > mapGetters. 
+// Методы, которые позводяют смапить какие-то сущности из Store
+import { mapGetters, mapActions } from 'vuex';
+import PostForm from './components/PostForm'
+export default {
+  name: 'App',
+  // computed: {
+  //   allPosts() {
+  //     // специальны объект из Vue: $store
+  //     return this.$store.getters.allPosts;
+  //   }
+  // },
+
+  // вариант работы с Vuex
+  computed: mapGetters(['allPosts', 'postsCount']),
+  methods: mapActions(['fetchPosts']),
+  components: { PostForm },
+  async mounted() {
+    // this.$store.dispatch('fetchPosts')
+    this.fetchPosts();
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  width: 400px;
+}
+
+.post {
+  border: 1px solid #ccc;
+  border-radius:  5px;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ src/store/modules/post.js
@@ -0,0 +1,40 @@
+export default {
+    // в cations можем пользоваться асинхронными событиями
+    // ctx - context
+    actions: {
+        async fetchPosts(ctx, limit = 3) {
+            const res = await fetch(
+            'https://jsonplaceholder.typicode.com/posts?_limit=' + limit
+          )
+          const posts = await res.json()
+          
+          ctx.commit('updatePosts', posts)
+         }
+    },
+    // только синхронные методы. State меняется через mutations c помощью мутации
+    mutations: {
+        updatePosts(state, posts) {
+            state.posts = posts
+        },
+        createPost(state, newPost){
+            state.posts.unshift(newPost)
+        }
+    },
+    state: {
+        posts: []
+    },
+    getters: {
+        validPosts(state){
+            return state.posts.filter(p => {
+                return p.title && p.body
+            })
+        },
+        allPosts(state) {
+            return state.posts
+        },
+        // как обратиться одному геттеру к другому
+        postsCount(state, getters){
+            return state.validPosts.length
+        }
+    }
+}
